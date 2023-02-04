006db4e4886a6b3cf0bb6dc3ebf22fcfb673317a
Preston Hales <PrestongHales@gmail.com>
2023-02-03 20:13:23 -0700

    Make Second Page UI

---
+++ app/src/main/java/com/app1/main/ShowProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.app1.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_1.R
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
